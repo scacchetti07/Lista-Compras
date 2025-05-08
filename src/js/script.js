2cbf28986de290ae1b979d09e07fc4a36884e4e2
Scacchetti <luiscacchetti07@gmail.com>
2025-05-08 20:07:01 -0300

    Mostrando no HTML os itens adicionados a lista

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,10 @@
+import { criarListaDeCompras, criarComprados } from './createHTML.js';
+
 let listaDeItens = []
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-de-itens");
 
 // Não é necessário colocar um id no botão se ele estiver dentro de uma tag form e sendo do tip "submit"
 form.addEventListener("submit", adicionarItens)
@@ -10,6 +13,7 @@ function adicionarItens(evento) {
     // preventDefault() => faz com que o conteúdo se mantenha no formulário após o clique do botão.
     evento.preventDefault();
     salvarItem();
+    mostrarItem();
 }
 
 function salvarItem() {
@@ -22,8 +26,25 @@ function salvarItem() {
         alert(`O item ${comprasItem.toUpperCase()} já existe na lista. Tente Adicionar outro!`);
         return;
     }
+    
     // Atribuí o objeto com propriedade nomeItem para o Array listaDeItens
     listaDeItens.push({
-        nomeItem: comprasItem
+        nomeItem: comprasItem.charAt(0).toUpperCase() + comprasItem.slice(1)
     });
+}
+
+function mostrarItem() {
+    limparLista(ulItens); // a chamada desse método impede que os itens sejam inseridos 2x no HTML
+
+    // foreach() => método para manipular elementos dentro de uma array (método callback)
+    listaDeItens.forEach((elem, index) => {
+        ulItens.appendChild(criarListaDeCompras(elem.nomeItem, index));
+    });
+}
+
+// Essa função irá limpar todos os Childs existentes na lista
+function limparLista(list) {
+    while (list.firstChild) {
+        list.removeChild(ulItens.firstChild);
+    }
 }
